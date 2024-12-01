875bfee582f85b7a83d1ff489baa6ba2f978c804
matheusheusmat <matheushponte@gmail.com>
2024-12-01 17:24:02 -0300

    Testes automatizados de senhas

--- test/password_service_test.dart
+++ test/password_service_test.dart
@@ -4,24 +4,70 @@ import 'package:mc656finalproject/services/password_service.dart';
 
 void main() {
   group('Verificação de senha forte', () {
-    test('Senha é forte se todos as condições forem validadas', () {
-      expect(PasswordService.isStrongPassword('Teste123!'), true);
+
+    test('Caso 1 - Senha válida: tam >= 8, min 1 num, min 1 let, min 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('abcd123@'), true);
     });
 
-    test('Senha é fraca quando possui menos de 8 caracteres', () {
-      expect(PasswordService.isStrongPassword('Tes1!'), false);
+    test('Caso 2 - Senha inválida: tam < 8, 0 num, 0 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('_______'), false);
     });
 
-    test('Senha é fraca quando não possui números', () {
-      expect(PasswordService.isStrongPassword('Teste!!!'), false);
+    test('Caso 3 - Senha inválida: tam < 8, 0 num, 0 let, 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('!@#.?!:'), false);
     });
 
-    test('Senha é fraca quando não possui caracteres especiais', () {
-      expect(PasswordService.isStrongPassword('Teste1234'), false);
+    test('Caso 4 - Senha inválida: tam < 8, 0 num, min 1 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('abcdefg'), false);
     });
 
-    test('Senha é fraca quando não possui letras', () {
-      expect(PasswordService.isStrongPassword('12345678!'), false);
+    test('Caso 5 - Senha inválida: tam < 8, 0 num, min 1 let, min 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('abcdef!'), false);
     });
+
+    test('Caso 6 - Senha inválida: tam < 8, min 1 num, 0 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('8765432'), false);
+    });
+
+    test('Caso 7 - Senha inválida: tam < 8, min 1 num, 0 let, min 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('1!2@3#4'), false);
+    });
+
+    test('Caso 8 - Senha inválida: tam < 8, min 1 num, min 1 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('abcde34'), false);
+    });
+
+    test('Caso 9 - Senha inválida: tam < 8, min 1 num, min 1 let, min 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('qwert!6'), false);
+    });
+
+    test('Caso 10 - Senha inválida: tam >= 8, 0 num, 0 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('________'), false);
+    });
+
+    test('Caso 11 - Senha inválida: tam >= 8, 0 num, 0 let, min 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('!@#!@#!@'), false);
+    });
+
+    test('Caso 12 - Senha inválida: tam >= 8, 0 num, min 1 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('qwertyui'), false);
+    });
+
+    test('Caso 13 - Senha inválida: tam >= 8, 0 num, min 1 let, min 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('testand@'), false);
+    });
+
+    test('Caso 14 - Senha inválida: tam >= 8, min 1 num, 0 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('20240101'), false);
+    });
+
+    test('Caso 15 - Senha inválida: tam >= 8, min 1 num, 0 let, min 1 carac. esp', () {
+      expect(PasswordService.isStrongPassword('1!2@3#2@'), false);
+    });
+
+    test('Caso 16 - Senha inválida: tam >= 8, min 1 num, min 1 let, 0 carac. esp', () {
+      expect(PasswordService.isStrongPassword('mrjel200'), false);
+    });
+
   });
 }
